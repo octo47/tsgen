73f960d938a41acc500dee06b8d1a0109ba65a54
Andrey Stepachev <sandrey@dropbox.com>
2016-07-10 12:25:07 +0100

    adding increasing generator

--- increasing.go
+++ increasing.go
@@ -1,1 +1,20 @@
 package tsgen
+
+import "math/rand"
+
+// IncreasingTimeSeries implements random walk.
+type IncreasingTimeSeries struct {
+	Generator
+	last Point
+}
+
+func NewIncreasingTimeSeries(r *rand.Rand, tags []Tag) *IncreasingTimeSeries {
+	return &IncreasingTimeSeries{Generator{rnd: r, tags: tags}, Point{}}
+}
+
+func (rw *IncreasingTimeSeries) Next(points *[]Point) {
+	for i := range *points {
+		rw.last.Value += rw.rnd.Float64()
+		(*points)[i] = rw.last
+	}
+}
--- increasing_test.go
+++ increasing_test.go
@@ -1,7 +1,6 @@
 package tsgen
 
 import (
-	"math"
 	"math/rand"
 	"testing"
 
@@ -9,25 +8,24 @@ import (
 )
 
 // Hook up gocheck into the "go test" runner.
-func TestRandomWalk(t *testing.T) { TestingT(t) }
+func TestIncreasing(t *testing.T) { TestingT(t) }
 
-type RandomWalkTimeSeriesSuite struct {
+type IncreasingTimeSeriesSuite struct {
 	tags []Tag
 }
 
-var _ = Suite(&RandomWalkTimeSeriesSuite{
+var _ = Suite(&IncreasingTimeSeriesSuite{
 	tags: []Tag{Tag{"tag1", "value1"}},
 })
 
-func (s *RandomWalkTimeSeriesSuite) TestRandomWalk(c *C) {
+func (s *IncreasingTimeSeriesSuite) TestIncreasing(c *C) {
 	rnd := rand.New(rand.NewSource(1))
-	rw := NewRandomWalkTimeSeries(rnd, s.tags)
+	rw := NewIncreasingTimeSeries(rnd, s.tags)
 	c.Assert(rw.Tags(), DeepEquals, s.tags)
 	points := make([]Point, 10)
 	rw.Next(&points)
 	for i := 0; i < len(points)-2; i++ {
 		diff := points[i+1].Value - points[i].Value
-		c.Assert(math.Abs(diff) < 0.5, Equals, true,
-			Commentf("Diff should be in range [-0.5;0.5]"))
+		c.Assert(diff >= 0, Equals, true, Commentf("Diff should be positive: %v", diff))
 	}
 }
